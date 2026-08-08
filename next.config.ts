7c98a132a43c3d9585077188f7b37942f2a7d92b
Harshit Goyal <jaigoel2206@gmail.com>
2026-08-08 15:37:36 +0530

    fix: remove output standalone from next.config.ts to fix Vercel ENOENT next-server.js.nft.json build error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   compress: true,
-  output: "standalone",
   productionBrowserSourceMaps: false,
   reactStrictMode: true,
   images: {
